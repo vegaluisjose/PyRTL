c1696790a070c2bff7f80973310b0aa938264175
Luis Vega <vegaluisjose@gmail.com>
2021-07-23 20:44:38 -0700

    refactor

--- pyrtl/importexport.py
+++ pyrtl/importexport.py
@@ -1586,37 +1586,31 @@ def output_to_reticle(open_file, block=None):
             f.write("{}{}:i{} = const[{}];\n".format(indent, new, const.bitwidth, const.val))
 
     for log_net in _net_sorted(block.logic_subset()):
+        arg_name = namer.rename(log_net.args[0].name)
+        dst_name = namer.rename(log_net.dests[0].name)
+        dst = emit_expr(dst_name, log_net.dests[0].bitwidth)
         if log_net.op == "s" and isinstance(log_net.args[0], Const) and log_net.args[0].bitwidth == 1 and len(log_net.op_param) == 1:
-            arg_name = namer.rename(log_net.args[0].name)
             namer.bind(log_net.dests[0].name, arg_name)
         elif log_net.op == "s" and isinstance(log_net.args[0], Const) and log_net.args[0].bitwidth == 1 and len(log_net.op_param):
-            arg_name = namer.rename(log_net.args[0].name)
-            dst_name = namer.rename(log_net.dests[0].name)
             if arg_name == const_f:
                 f.write("{}{} = const[0];\n".format(indent, emit_expr(dst_name, len(log_net.op_param))))
             else:
                 arg = [arg_name for _ in log_net.op_param]
-                dst = emit_expr(dst_name, log_net.dests[0].bitwidth)
                 f.write("{}{} = cat({});\n".format(indent, dst, ", ".join(arg)))
         elif log_net.op == "s":
-            arg_name = namer.rename(log_net.args[0].name)
             cat = []
             for i in log_net.op_param:
                 new = namer.new()
                 cat.append(new)
                 dst = emit_expr(new, 1)
                 f.write("{}{} = ext[{}]({});\n".format(indent, dst, i, arg_name))
-            dst_name = namer.rename(log_net.dests[0].name)
-            dst = emit_expr(dst_name, log_net.dests[0].bitwidth)
             f.write("{}{} = cat({});\n".format(indent, dst, ", ".join(cat)))
         elif log_net.op == "w" and isinstance(log_net.dests[0], Output):
-            arg_name = namer.rename(log_net.args[0].name)
-            dst = emit_expr(log_net.dests[0].name, log_net.dests[0].bitwidth)
-            f.write("{}{} = id({});\n".format(indent, dst, arg_name))
+            out = emit_expr(log_net.dests[0].name, log_net.dests[0].bitwidth)
+            f.write("{}{} = id({});\n".format(indent, out, arg_name))
         elif log_net.op == "w":
-            arg_name = namer.rename(log_net.args[0].name)
-            dst_name = namer.rename(log_net.dests[0].name)
-            dst = emit_expr(dst_name, log_net.dests[0].bitwidth)
             f.write("{}{} = id({});\n".format(indent, dst, arg_name))
+        elif log_net.op == "r":
+            f.write("{}{} = reg[0]({}, {});\n".format(indent, dst, arg_name, const_t))
     f.write("}")
     return 0
