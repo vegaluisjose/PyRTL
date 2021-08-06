79eefef2ee301159ce2c837a3a075eae23807a7c
Luis Vega <vegaluisjose@gmail.com>
2021-08-06 14:40:19 -0700

    update

--- examples/reticle/aes.py
+++ examples/reticle/aes.py
@@ -7,19 +7,18 @@ aes = AES()
 plaintext = pyrtl.Input(bitwidth=128, name='aes_plaintext')
 key = pyrtl.Input(bitwidth=128, name='aes_key')
 aes_ciphertext = pyrtl.Output(bitwidth=128, name='aes_ciphertext')
-reset = pyrtl.Input(1, name='reset')
+aes_reset = pyrtl.Input(1, name='reset')
 ready = pyrtl.Output(1, name='ready')
-ready_out, aes_cipher = aes.encrypt_state_m(plaintext, key, reset)
+ready_out, aes_cipher = aes.encrypt_state_m(plaintext, key, aes_reset)
 ready <<= ready_out
 aes_ciphertext <<= aes_cipher
 
 pyrtl.optimize()
 
-with io.StringIO() as vfile:
-    output_to_reticle(vfile, vfile)
-    print(vfile.getvalue())
+# Reticle
+with open('aes.ir', 'w') as f1, open('aes.json', 'w') as f2:
+    output_to_reticle(f1, f2)
 
-#print("\n\n\n\n")
-#with io.StringIO() as vfile:
-#    pyrtl.output_to_verilog(vfile)
-#    print(vfile.getvalue())
+# Verilog
+with open('main.v', 'w') as f:
+   pyrtl.output_to_verilog(f)
